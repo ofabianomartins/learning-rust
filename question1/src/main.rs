3943a512d2014059b8936baece279f306de2e703
Fabiano Martins <fabiano.paula.martins@gmail.com>
2026-08-27 05:59:01 -0300

    feat: atualize o Box de dados

--- question1/src/main.rs
+++ question1/src/main.rs
@@ -5,64 +5,80 @@ enum Node {
     Nil
 }
 
-fn insert(root: &mut Node, elem: i32) {
-    if let Node::Elem(_, child) = root {
+fn new() -> Box<Node> {
+    return Box::new(Node::Nil);
+}
+
+fn insert(root: &mut Box<Node>, elem: i32) {
+    if let Node::Elem(_, child) = root.as_mut() {
         insert(child, elem);
-    } else if let Node::Nil = root {
-        *root = Node::Elem(elem, Box::new(Node::Nil));
+    } else {
+        *root = Box::new(Node::Elem(elem, Box::new(Node::Nil)));
     }
 }
 
-fn remove(root: &mut Node) {
-    if let Node::Elem(_, child) = root {
-        if let Node::Nil = **child {
-            *root = Node::Nil;
-        } else {
-            remove(child);
-        }
+fn remove(root: &mut Box<Node>) {
+    // 1. Caso base: lista vazia
+    if let Node::Nil = root.as_ref() {
+        return;
+    }
+
+    // 2. Verifica se o filho imediato é o último nó (Nil)
+    let is_child_nil = match root.as_ref() {
+        Node::Elem(_, child) => matches!(child.as_ref(), Node::Nil),
+        Node::Nil => false,
+    };
+
+    if is_child_nil {
+        // Transforma o nó em Nil
+        *root = Box::new(Node::Nil);
+    } else if let Node::Elem(_, child) = root.as_mut() {
+        // Caso contrário, continua descendo recursivamente
+        remove(child);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::Node;
+    use crate::new;
     use crate::insert;
     use crate::remove;
 
     #[test]
     fn test_insert_empty_list() {
-        let mut root: Node = Node::Nil;
+        let mut root: Box<Node> = new();
         insert(&mut root, 10);
-        assert_eq!(root, Node::Elem(10, Box::new(Node::Nil)));
+        assert_eq!(root, Box::new(Node::Elem(10, Box::new(Node::Nil))));
     }
 
     #[test]
     fn test_insert_one_value_list() {
-        let mut root: Node = Node::Nil;
+        let mut root: Box<Node> = new();
         insert(&mut root, 10);
         insert(&mut root, 12);
         assert_eq!(
             root,
-            Node::Elem(10, Box::new(Node::Elem(12, Box::new(Node::Nil))))
+            Box::new(Node::Elem(10, Box::new(Node::Elem(12, Box::new(Node::Nil)))))
         );
     }
 
     #[test]
     fn test_remove_from_empty() {
-        let mut root: Node = Node::Nil;
+        let mut root: Box<Node> = new();
         remove(&mut root);
-        assert_eq!(root, Node::Nil);
+        assert_eq!(root, Box::new(Node::Nil));
     }
 
     #[test]
     fn test_insert_one_value_list_and_remove() {
-        let mut root: Node = Node::Nil;
+        let mut root: Box<Node> = new();
         insert(&mut root, 10);
         insert(&mut root, 12);
         remove(&mut root);
         assert_eq!(
             root,
-            Node::Elem(10, Box::new(Node::Nil))
+            Box::new(Node::Elem(10, Box::new(Node::Nil)))
         );
     }
 
